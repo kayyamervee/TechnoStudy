9fd33ad2dcd59d31b2663f9de6946172f23d12b2
songul-cam <songulcam9369@gmail.com>
2024-10-13 23:54:54 +0300

    US_CrossToCampus is completed.

--- src/test/java/US_CrossToCampus.java
+++ src/test/java/US_CrossToCampus.java
@@ -1,2 +1,27 @@
-public class US_CrossToCampus {
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+import pages.CrossToCampus_POM;
+import utility.BaseDriver;
+import utility.ConfigReader;
+
+public class US_CrossToCampus extends BaseDriver {
+    @Test()
+    public void crossToCampus() {
+        CrossToCampus_POM elements = new CrossToCampus_POM();
+
+        driver.get(ConfigReader.getProperty("URL"));
+        wait.until(ExpectedConditions.urlToBe(ConfigReader.getProperty("URL")));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.campusLogin));
+        elements.campusLogin.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.userName));
+        elements.userName.sendKeys(ConfigReader.getProperty("userName"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.passWord));
+        elements.passWord.sendKeys(ConfigReader.getProperty("passWord"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.button));
+        elements.button.click();
+    }
 }
