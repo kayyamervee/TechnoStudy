a4309a0816f486bb54fd42a6a3a6ed013ac1e29f
SefaKahramannn <sefakahraman47ak@gmail.com>
2024-10-15 09:49:50 +0300

    Drop-down menu finished

--- src/test/java/pages/Dropdown_POM.java
+++ src/test/java/pages/Dropdown_POM.java
@@ -1,4 +1,21 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utility.BaseDriver;
+
+import java.util.List;
+
 public class Dropdown_POM {
+
+    public Dropdown_POM() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+
+    @FindBy(linkText = "Kurslar")
+    public WebElement coursesBtn;
+
+    @FindBy(xpath = "//span[contains(@class,'link-inner_left')]")
+    public List<WebElement> courses;
 }
