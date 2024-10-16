4f45c09376f6a05ad88a5e81c0f2061eb37649da
songul-cam <songulcam9369@gmail.com>
2024-10-16 10:57:41 +0300

    US_TermsOfUse.java has been updated.

--- src/test/java/US_TermsOfUse.java
+++ src/test/java/US_TermsOfUse.java
@@ -1,2 +1,80 @@
-public class US_TermsOfUse {
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TermsOfUse_POM;
+import utility.BaseDriver;
+import utility.ConfigReader;
+
+public class US_TermsOfUse extends BaseDriver {
+    @Test()
+    public void TermsOfUse(){
+        TermsOfUse_POM elements=new TermsOfUse_POM();
+
+        driver.get(ConfigReader.getProperty("URL"));
+        wait.until(ExpectedConditions.urlToBe(ConfigReader.getProperty("URL")));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.nameSurname));
+        elements.nameSurname.sendKeys(ConfigReader.getProperty("nameSurname"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.eMail));
+        elements.eMail.sendKeys(ConfigReader.getProperty("eMail"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.countryCode));
+        elements.countryCode.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.coDe));
+        elements.coDe.click();
+
+        wait.until(ExpectedConditions.visibilityOf(elements.coDe));
+        Assert.assertTrue(elements.coDe.getText().contains("The country code was not entered correctly."));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.phoneNumber));
+        elements.phoneNumber.sendKeys(ConfigReader.getProperty("phoneNumber"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.yourAge));
+        elements.yourAge.sendKeys(ConfigReader.getProperty("yourAge"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.yourProfession));
+        elements.yourProfession.sendKeys(ConfigReader.getProperty("yourProfession"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.educationStatus));
+        elements.educationStatus.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.univerSity));
+        elements.univerSity.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.selectYourCountry));
+        elements.selectYourCountry.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.argenTina));
+        elements.argenTina.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.selectYourCourse));
+        elements.selectYourCourse.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.sDet));
+        elements.sDet.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.whereDidYouHearAboutUs));
+        elements.whereDidYouHearAboutUs.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.linkedLn));
+        elements.linkedLn.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.checkbox));
+        elements.checkbox.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.termsOfUse));
+        elements.termsOfUse.click();
+
+        wait.until(ExpectedConditions.visibilityOf(elements.termsOfUse));
+        Assert.assertTrue(elements.termsOfUse.getText().contains("The terms of use were not read."));
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.send));
+        elements.send.click();
+
+        wait.until(ExpectedConditions.visibilityOf(elements.send));
+        Assert.assertTrue(elements.send.getText().contains("Contact us was not filled out."));
+
+    }
 }
--- src/test/java/pages/TermsOfUse_POM.java
+++ src/test/java/pages/TermsOfUse_POM.java
@@ -1,4 +1,72 @@
 package pages;
 
-public class TermsOfUse_POM {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utility.BaseDriver;
+
+public class TermsOfUse_POM{
+    public TermsOfUse_POM(){PageFactory.initElements(BaseDriver.driver,this);
+    }
+    @FindBy(xpath = "//input[@placeholder='Name Surname']")
+    public WebElement nameSurname;
+
+    @FindBy(name = "[name=email]")
+    public WebElement eMail;
+
+    @FindBy(xpath = "//span[@class='t-input-phonemask__select-flag']")
+    public WebElement countryCode;
+
+    @FindBy(id = "[t-phonemask_ar]")
+    public WebElement coDe;
+
+    @FindBy(id = "[input_1661174990146]")
+    public WebElement phoneNumber;
+
+    @FindBy(name = "[Input]")
+    public WebElement yourAge;
+
+    @FindBy(name = "[Input_2]")
+    public WebElement yourProfession;
+
+    @FindBy(id = "[sb-1667664755026]")
+    public WebElement educationStatus;
+
+    @FindBy(xpath = "//option[@value='Üniversite']")
+    public WebElement univerSity;
+
+    @FindBy(id = "[sb-1714463229186]")
+    public WebElement selectYourCountry;
+
+    @FindBy(xpath = "//option[@value='Argentina']")
+    public WebElement argenTina;
+
+    @FindBy(id = "[sb-1663337581601]")
+    public WebElement selectYourCourse;
+
+    @FindBy(css = "option[value='SDET Türkçe']")
+    public WebElement sDet;
+
+    @FindBy(id = "[sb-1670423010572]")
+    public WebElement whereDidYouHearAboutUs;
+
+    @FindBy(css = "option[value='LinkedIn']")
+    public WebElement linkedLn;
+
+    @FindBy(xpath = "//div[@class='t-checkbox__indicator']")
+    public WebElement checkbox;
+
+    @FindBy(xpath = "//font[text()='Terms of Use']")
+    public WebElement termsOfUse;
+
+    @FindBy(xpath = "//font[text()='Send']")
+    public WebElement send;
+
+
+
+
+
+
+
+
 }
